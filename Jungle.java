9b219460924a5fca26b79b5e6db3cec0016b943a
Ivanna chazaro <ivannachazaro7@gmail.com>
2022-05-12 11:37:21 -0700

    Ivanna

--- Jungle.java
+++ Jungle.java
@@ -13,28 +13,18 @@ class Jungle extends Page
     void read()
     {
         System.out.println(
-<<<<<<< HEAD
+
 
 
         " Seven lucky Tributes have been dropped in an uncharted jungle. Only one can regin victorius.\n\n" +
 
         " The Tributes have been dropped in an uncharted jungle.\n\n" +
 
-        " Alex Russo, Shrek, Mrs.Incredible, Edward Cullen,Penelope Garcia, McLovin', Bill Ray Cyrus anxiously wait as the timer ticks down. \n\n" +
+        " Alex Russo, Shrek,Regina George, Edward Cullen,Penelope Garcia, McLovin', Bill Ray Cyrus anxiously wait as the timer ticks down. \n\n" +
         " The jungle is so quiet. Everyone can hear each others heartbeat. \n\n" +
-        "5 \n\n" + "4 \n\n" + "3 \n\n" + "2 \n\n" + "1"
-
-        "The Jungle\n\n" +
-=======
->>>>>>> bc66f038dc00eb4ebcd79e1987517bea2f1b5b11
-        " The Tributes have been dropped in an uncharted jungle./n" +
-<<<<<<< HEAD
-        " Alex Russo, Shrek, Mrs.Incredible, Edward Cullen,Penelope Garcia, McLovin', Bill Ray Cyrus anxiously wait as the timer ticks down"  
-
-=======
-        " Alex Russo, Shrek, Mrs.Incredible, Edward Cullen,Penelope Garcia, McLovin', Bill Ray Cyrus anxiously wait as the timer ticks down \n\n" + 
+        
         "5 \n\n" + "4 \n\n" + "3 \n\n" + "2 \n\n" + "1... \n\n" 
->>>>>>> bc66f038dc00eb4ebcd79e1987517bea2f1b5b11
+
         
         );
     }
--- Page1.java
+++ Page1.java
@@ -15,14 +15,10 @@ class Page1 extends Page
         
         "Everyone skatters into different directions. No one even dares to touch the supplies in the middle.\n\n" +
         "Shrek, from District DreamWorks, runs as fast as his legs could take him. He only thinks of his family. The midday sun makes the sand glare with blinding light.\n\n" +
-<<<<<<< HEAD
 
-        "\"Coming here was a bad idea. I miss me swamp. Rawrrrr. READ IN SCOTTISH ACCENT.\n\n" + "Shrek feeling hungry for some yummy waffles, made by his best friend donkey, is already tired of being in the humid jungle. Running and killing are not his favorite, especially since it is so much work, for a poor green ogre like himself."
 
-        "\"Coming here was a bad idea. I miss me swamp. Rawrrrr. (READ IN SCOTTISH ACCENT) \n\n" + "Shrek, feeling hungry for some yummy waffles, made by his best friend Donkey, is already tired of being in the humid jungle. Running and killing are not his favorite, especially since it is so much work, for a poor green ogre like himself."
-=======
-        "Coming here was a bad idea. I miss me swamp. Rawrrrr. (READ IN SCOTTISH ACCENT) \n\n" + "Shrek, feeling hungry for some yummy waffles, made by his best friend Donkey, is already tired of being in the humid jungle. Running and killing are not his favorite, especially since it is so much work, for a poor green ogre like himself."
->>>>>>> bc66f038dc00eb4ebcd79e1987517bea2f1b5b11
+        "Coming here was a bad idea. I miss me swamp. Rawrrrr. READ IN SCOTTISH ACCENT.\n\n" + "Shrek feeling hungry for some yummy waffles, made by his best friend donkey, is already tired of being in the humid jungle. Running and killing are not his favorite, especially since it is so much work, for a poor green ogre like himself."
+
          
         
 
